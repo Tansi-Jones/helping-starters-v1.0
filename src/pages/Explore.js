ba29acbbb1906c7dfc3813f6a41c680d1d67d44f
tansi-jones <tansijones@outlook.com>
2022-06-14 00:45:29 +0300

    update liked.js

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -20,7 +20,7 @@ export default function Explore() {
               />
             </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-center justify-center">
             {DataExplore.map(({ id, name, image, followers }) => {
               return (
                 <PrimaryCard
