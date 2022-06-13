5119430a3e327643023bd59606c67cc92050e0f8
tansi-jones <tansijones@outlook.com>
2022-06-14 01:50:24 +0300

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,48 +1,80 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import UserModal from "../models/user.js";
+import User from "../models/user.js";
 
-const secret = 'test';
-
-export const signin = async (req, res) => {
+export const signIn = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const oldUser = await UserModal.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-    if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+    if (!existingUser)
+      return res.status(404).json({ message: "User doesn't exist" });
 
-    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
 
-    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
 
-    res.status(200).json({ result: oldUser, token });
-  } catch (err) {
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(200).json({
+      status: "Success",
+      message: "✅ Signed In Success",
+      data: existingUser,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "❌ SignIn Failed",
+      data: error,
+    });
   }
 };
 
-export const signup = async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
+export const signUp = async (req, res) => {
+  const { email, password, name } = req.body;
 
   try {
-    const oldUser = await UserModal.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-    if (oldUser) return res.status(400).json({ message: "User already exists" });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    const result = User.create({
+      email,
+      password: hashedPassword,
+      name: `${name} `,
+    });
 
-    const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
 
-    res.status(201).json({ result, token });
+    res.status(200).json({
+      status: "Success",
+      message: "✅ Account created",
+      data: existingUser,
+      token,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-    
-    console.log(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "❌ Account creation Failed",
+      data: error,
+    });
   }
 };
