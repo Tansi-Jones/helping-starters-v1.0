c83be0feb9971847bc13eae5110663965b51bc9d
tansi-jones <tansijones@outlook.com>
2022-06-14 14:00:40 +0300

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,12 @@
 import { useForm } from "react-hook-form";
 import { IoMdHeart } from "react-icons/io";
 import Explore from "../assets/explore.png";
+import Avatar from "../assets/avatar.svg";
 
 export default function Profile() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
@@ -20,16 +20,17 @@ export default function Profile() {
       <main className="w-[90%] mx-auto pt-10 pb-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <section>
           <div className="relative max-w-[15rem] bg-white rounded-lg shadow-sm p-3">
-            <div className="relative overflow-clip h-40 rounded-lg">
-              <img src={Explore} alt="avatar" className=" object-cover" />
+            <div className="relative overflow-clip h-40 rounded-lg w-40 mx-auto">
+              <img src={Avatar} alt="avatar" />
             </div>
             <div className=" pt-5 pb-1">
-              <button
-                type="button"
-                className="border border-secondary rounded text-base py-2 px-2 w-full text-secondary"
+              <label
+                htmlFor="upload"
+                className="border border-secondary rounded text-base py-2 px-2 w-full text-secondary block text-center cursor-pointer"
               >
                 Upload Avatar
-              </button>
+              </label>
+              <input type="file" id="upload" className="hidden" />
             </div>
           </div>
         </section>
@@ -38,7 +39,7 @@ export default function Profile() {
         <section>
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="text-slate-800 space-y-4"
+            className="text-slate-800 space-y-5"
           >
             <div className="space-y-2">
               <p className="text-slate-500 text-sm ">Creator Handle</p>
@@ -46,7 +47,7 @@ export default function Profile() {
                 defaultValue="Kingjohn01"
                 {...register("handle")}
                 disabled
-                className="outline-none bg-slate-300 rounded text-base py-2 px-4 w-full text-slate-600 cursor-not-allowed"
+                className="outline-none bg-slate-200 rounded text-base py-2 px-4 w-full text-slate-600 cursor-not-allowed"
               />
             </div>
             <div className="space-y-2">
@@ -55,7 +56,7 @@ export default function Profile() {
                 defaultValue="John Mark"
                 {...register("name")}
                 disabled
-                className="outline-none bg-slate-300 rounded text-base py-2 px-4 w-full text-slate-600 cursor-not-allowed"
+                className="outline-none bg-slate-200 rounded text-base py-2 px-4 w-full text-slate-600 cursor-not-allowed"
               />
             </div>
             <div className="space-y-2">
@@ -64,22 +65,17 @@ export default function Profile() {
                 defaultValue="johnmark@gmail.com"
                 {...register("email")}
                 disabled
-                className="outline-none bg-slate-300 rounded text-base py-2 px-4 w-full text-slate-600 cursor-not-allowed"
+                className="outline-none bg-slate-200 rounded text-base py-2 px-4 w-full text-slate-600 cursor-not-allowed"
               />
             </div>
 
-            <div className="flex justify-between items-center gap-8">
+            <div className="flex justify-between items-center">
               <button
                 type="submit"
-                className="bg-secondary rounded text-base py-2 px-2 w-full text-white"
+                disabled
+                className="bg-secondary rounded text-base py-2 px-2 w-full text-white cursor-not-allowed"
               >
                 Save changes
-              </button>
-              <button
-                type="button"
-                className="border border-secondary rounded text-base py-2 px-2 w-full text-secondary"
-              >
-                Update password
               </button>
             </div>
           </form>
